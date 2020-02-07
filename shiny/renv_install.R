48c3dc2628f941ffeffacd37ae1673d044897b57
Robert DJ <git@dahl-jacobsen.dk>
2020-02-07 22:29:58 +0100

    Use external library

--- shiny/renv_install.R
+++ shiny/renv_install.R
@@ -1,6 +1,8 @@
-renv:::renv_paths_cache()
-
 renv::settings$external.libraries("/usr/local/lib/R/site-library")
+
+# Make sure renv knows of the new external library
+source(".Rprofile")
+
 renv::restore(confirm = FALSE)
 
 renv::isolate()
