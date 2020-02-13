41d34847cafe41b8fdf607bc37c624269bc50e3b
Robert DJ <git@dahl-jacobsen.dk>
2020-02-13 21:53:18 +0100

    Restore renv in two Docker runs

--- shiny_kmeans/renv_install.sh
+++ shiny_kmeans/renv_install.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env sh
 
 docker run --rm -it \
+    -v `pwd`:/home/shiny/project \
+    shiny-docker:latest Rscript -e 'renv::settings$external.libraries("/usr/local/lib/R/site-library")'
+
+docker run --rm -it \
     -e "RENV_PATHS_CACHE=/home/shiny/renv/cache" \
     -v /home/robert/Documents/R/renv-cache:/home/shiny/renv/cache \
     -v `pwd`:/home/shiny/project \
-    shiny-docker:latest Rscript renv_install.R
+    shiny-docker:latest Rscript -e "renv::restore(confirm = FALSE)" -e "renv::isolate()"
 
